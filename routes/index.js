a41a2f1497752365839b60826cb0eab3045d93fe
Taylor Chen <s68175@gmail.com>
2020-08-22 10:17:49 +0800

    Add login, register layout

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const home = require('./modules/home')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
 const restaurants = require('./modules/restaurants')
+const users = require('./users')
 
-router.use('/', home)
 router.use('/search', search)
 router.use('/sort', sort)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
+router.use('/', home)
 
 module.exports = router
