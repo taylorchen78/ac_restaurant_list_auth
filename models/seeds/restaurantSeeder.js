2c473e198f0601a5345597474b985a1e80f51cab
Taylor Chen <s68175@gmail.com>
2020-08-23 20:25:33 +0800

    Add restaurant seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,12 +1,65 @@
-// require packages
 const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json')
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
+
+const User = require('../user')
+
+const SEED_USER = [
+  {
+    name: '',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurant: [1, 2, 3]
+  },
+  {
+    name: '',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurant: [4, 5, 6]
+  }
+]
 
 db.once('open', () => {
-  restaurantList.results.forEach(element => {
-    Restaurant.create(element)
-  });
-
-  console.log('done')
+  Promise.all(Array.from({ length: 2 }, (_, i) => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER[i].password, salt))
+      .then(hash => User.create({
+        name: SEED_USER[i].name,
+        email: SEED_USER[i].email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        const list = []
+        restaurantList.results.forEach((element, index) => {
+          if (SEED_USER[i].restaurant.includes(element.id)) {
+            list.push(element)
+          }
+        })
+        return { list, userId }
+      })
+      .then(restaurant => {
+        return Promise.all(Array.from({ length: restaurant.list.length }, (_, j) =>
+          Restaurant.create({
+            name: restaurant.list[j].name,
+            name_en: restaurant.list[j].name_en,
+            category: restaurant.list[j].category,
+            image: restaurant.list[j].image,
+            location: restaurant.list[j].location,
+            phone: restaurant.list[j].phone,
+            google_map: restaurant.list[j].google_map,
+            rating: restaurant.list[j].rating,
+            description: restaurant.list[j].description,
+            userId: restaurant.userId
+          })
+        ))
+      })
+  }))
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
